2dd13d1071400eb0226f37f831b47b0060a3b289
Sourav Datta <soura.jagat@gmail.com>
2020-04-12 17:30:16 +0530

    Initial Commit

---
+++ DailyTasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyTasks
+//
+//  Created by Sourav Datta on 12/04/20.
+//  Copyright © 2020 10Avenue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
